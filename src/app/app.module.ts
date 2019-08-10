31711ef90f56ee6d61b151a1b1eee884b5fbfb6c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-10 01:22:35 -0400

    Added the recipies component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
    declarations: [
@@ -15,7 +16,8 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
       ShoppingListComponent,
       ShoppingListEditComponent,
       RecipeListComponent,
-      RecipeDetailComponent
+      RecipeDetailComponent,
+      RecipesComponent
    ],
    imports: [
       BrowserModule
