e1e04fd687a94e6340e0a967b69e5c460e59bc72
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-16 22:40:30 -0400

    Moved the handling of the recipe array to the service.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,16 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    //   Returns a new copy of the original array.
+    return this.recipes.slice();
+  }
+}
