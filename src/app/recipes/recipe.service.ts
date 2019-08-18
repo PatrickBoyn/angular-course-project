b1c250660c7bb5b9410c928de816a609c89b6671
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-18 17:27:28 -0400

    Injected the ingredients service, and added a method.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
@@ -13,8 +15,13 @@ export class RecipeService {
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     //   Returns a new copy of the original array.
     return this.recipes.slice();
+  }
+  addIngredientsToSL(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
