70175a141928718d3768d69f338020c44541d7cc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-18 00:58:58 -0400

    Added an ingredient list to the Recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,15 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg'
+      'Soup',
+      'A tasty soup',
+      'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg',
+      [new Ingredient('Tomatoes', 5), new Ingredient('Carrots', 4), new Ingredient('Onions', 5)]
     ),
   ];
 
