6d9241e7b5cb629ee6101d7b77e72d1df968ed48
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-25 15:27:38 -0400

    Removed the navigate function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-course-project';
-  loadedFeature = 'recipe';
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+  title = 'Recipe Maker';
 }
