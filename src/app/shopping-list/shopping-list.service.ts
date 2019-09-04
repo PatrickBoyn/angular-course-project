81c20021a8f34b289d7b0c8cc54004eaeac9c540
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-04 02:24:09 -0400

    Added a getIngredient method.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -10,6 +10,10 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
