f63b96774b6c9e48b3214fc4a3fc122085c7cf13
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-05 15:35:21 -0400

    Added a method to delete a recipe.

--- src/app/recipes/recipe-list/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-list/recipe-detail/recipe-detail.component.ts
@@ -12,7 +12,11 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
@@ -27,5 +31,9 @@ export class RecipeDetailComponent implements OnInit {
 
   onEditRecipe() {
     this.router.navigate(['edit'], { relativeTo: this.route });
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
   }
 }
